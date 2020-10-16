866d19604197cf2747e46232d417d30173164726
Nir Kahana <nirkahana8@gmail.com>
2020-10-17 02:24:12 +0300

    score feature

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./App.css";
 import Map from './components/Map'
-
-
 function App() {
-
+  
   return (
     <>
       <Map />
--- my-app/src/components/Map.js
+++ my-app/src/components/Map.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
+import api_key from '../myEnv';
+import { motion } from "framer-motion"
+// import {useSpring, animated} from 'react-spring'
+// import * as easings from 'd3-ease'
 import israeliSettelments from '../cities.json'
 import {
   GoogleMap,
   LoadScript,
   Marker,
 } from "@react-google-maps/api";
-import axios from 'axios'
 // require("dotenv").config();
 const editedCities = israeliSettelments.map((settlement) => {return {name: settlement.MGLSDE_L_4, population: settlement.MGLSDE_L_1, lat: settlement.Y, lng: settlement.X}})
-let citiesList = editedCities.filter((city) => city.population > 200000);
+let citiesList = editedCities.filter((city) => city.population > 10000);
+const originalLength = citiesList.length;
 
 function MapGame() {
   
@@ -16,10 +20,11 @@ function MapGame() {
     lat: 31.4,
     lng: 34.7
   };
+  let turnNumber = originalLength-citiesList.length;
   // const getRandomIndex = (arr) => Math.floor(Math.random()*(arr.length));
   const drawCity = () => {
     if (citiesList.length === 0) {
-      setCurrentCity(null)
+      setCurrentCity("game over")
       return "game over"
     }
     const randomIndex = Math.floor(Math.random()*(citiesList.length));
@@ -30,10 +35,30 @@ function MapGame() {
   }
 
   const [markerPosition, setMarkerPosition] = useState(defaultCenter);
-  const [mapCenter, setMapCenter] = useState(defaultCenter);
+  const [mapCenter, setMapCenter] = useState(defaultCenter); /// for some reason is necessary
   const [currentCity, setCurrentCity] = useState(null)
   const [points, setPoints] = useState(0);
-    
+  const [score, setScore] = useState(null);
+
+  const scoreRef = useRef();
+
+  // const props = useSpring({
+  //   config: { 
+  //     duration: 500,
+  //     // easing: easings.easePolyOut.exponent(1),
+  //     easing: easings.easeQuadOut,
+  //   },
+  //   from: { opacity: 0, fontSize: "1.25em"},
+  //   // to: [
+  //   //   {opacity: 1, fontSize: "1.5em"}, 
+  //   //   {opacity: 0, fontSize: "1.5em"}
+  //   // ],
+  //   to: async (next, cancel) => {
+  //     await next({opacity: 1, fontSize: "1.4em"})
+  //     await next({opacity: 0, fontSize: "1.5em"})
+  //   },
+  // })
+
   useEffect(() => {
     console.log(citiesList.length);
     drawCity()
@@ -45,7 +70,8 @@ function MapGame() {
 
   const mapStyles = {
     height: "100vh",
-    width: "80%",
+    width: "100%",
+    display: "inline-block"
   };
 
   const onClicked = (e) => {
@@ -54,15 +80,15 @@ function MapGame() {
       lng: e.latLng.lng()
     }
     setMarkerPosition(position)
-    if (currentCity !== null) {
+    if (currentCity !== "game over") {
       const d = clacDistance(position, currentCity)
       drawCity()
       const score = calcPoints(d)
-      console.log("previous city is: ", currentCity.name);
-      console.log('distance is: ', d);
-      // console.log('turns played: ', turnsPlayed+1);
-      console.log('score is: ', score);
-      setPoints(points + score)
+      setScore(`+${score.toFixed()}`);
+      setPoints(points + score);
+      // console.log("previous city is: ", currentCity.name);
+      // console.log('distance is: ', d);
+      // console.log('score is: ', score);
     }
   }
 
@@ -85,11 +111,20 @@ function MapGame() {
   function deg2rad(deg) {
     return deg * (Math.PI/180)
   }
-  const calcPoints = (distance) => 100-(100*distance/(100 + distance))
+  const calcPoints = (distance) => 100-(100*distance/(75 + distance))
+
+  const getScore = () => {
+    setTimeout(()=>{
+      setScore(null);
+    },1500)
+    return <div className="score" ref={scoreRef}>{score}</div>
+  }
 
   return (
+    currentCity ? 
     <>
-      <LoadScript googleMapsApiKey="AIzaSyDPYiDA_pLwFMRMF0D0wQi8V75rxAy964Y">
+    <div className="flex-container">
+      <LoadScript googleMapsApiKey={api_key}>
         <GoogleMap mapContainerStyle={mapStyles} 
                    zoom={7.5}
                    center={mapCenter}
@@ -102,11 +137,25 @@ function MapGame() {
                     draggable={true}
             />
           }
-
+              <div className="info-box">
+                <div>{currentCity.name ? `#${turnNumber} ${currentCity.name}` : "game over"}</div>
+                <div>Points: {points.toFixed()}</div>
+                {score && 
+                <motion.div animate={{ opacity: [0,1,1,0] }} 
+                            initial={{opacity: 0}} 
+                            transition={{ ease: "easeOut", duration: 1 }} 
+                            onAnimationComplete={() => setScore(null)}
+                >
+                  {score}
+                </motion.div>}
+                  {/* <animated.h1 style={props}>hello</animated.h1> */}
+              </div>
         </GoogleMap>
       </LoadScript>
-      <span>{points}</span>
+    </div>
     </>
+    :
+    <div>Loading...</div>
   );
 }
 
